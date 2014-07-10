b79414b504e5fa31cbaa8609a26bf61242d801ae
William Denton <wtd@pobox.com>
2014-07-09 22:13:42 -0400

    Put under GPL v3

--- cbcappearances.rb
+++ cbcappearances.rb
@@ -1,5 +1,23 @@
 #!/usr/bin/env ruby
 
+# This file is part of CBC Appearances.
+#
+# CBC Appearances is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# CBC Appearances is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with CBC Appearances.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Copyright 2014 William Denton
+
+
 require 'date'
 require 'open-uri'
 require 'json'
