74c107b0556b1f8903a2b28ed03344118aca7317
William Denton <wtd@pobox.com>
2014-12-29 12:59:26 -0500

    Minor tweaking to R script

--- host-activity.R
+++ host-activity.R
@@ -1,23 +1,29 @@
 #!/usr/bin/env Rscript
 
 suppressMessages(library(dplyr))
-library(reshape)
+library(reshape2)
 
-a <- read.csv("appearances.csv")
+a <- read.csv("appearances.csv", header = TRUE)
 
 a$date <-as.Date(a$date)
 
-print(paste("Date range: ", min(a$date), " - ", max(a$date)))
+print(paste("Date range: ", min(a$date), "-", max(a$date)))
 
-b <- a %>% select(name, fee) %>% group_by(name, fee) %>% summarise(count=n())
+b <- a %>% select(name, fee) %>% group_by(name, fee) %>% summarise(count = n())
 
-appearances <- cast(b, name~fee, fill = 0, value = "count")
+appearances <- dcast(b, name ~ fee, fill = 0, value.var = "count")
 
 appearances <- mutate(appearances, Total = Expenses + Paid + Unpaid, Paid.PerCent = 100 * round(Paid / Total, digits = 2))
 
+print("How many people are on the list?")
+nrow(appearances)
+
+print("How many appearances were made in total?")
+sum(appearances$Total)
+
 print("Define as being 'busy' anyone who has more than this many appearances:")
 
-busy.number <- mean(appearances$Total)
+busy.number <- round(mean(appearances$Total) + sd(appearances$Total), 0)
 busy.number
 
 print("Who is busy?")
